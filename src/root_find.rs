219b4003657ae99e1c6996bee52da0f2bd3f5da6
Philpax <me@philpax.me>
2016-06-04 18:33:24 +1000

    Implement a ray marching method

--- src/root_find.rs
+++ src/root_find.rs
@@ -27,14 +27,38 @@ pub fn secant<F>(mut x0: f32,
     Some(x1)
 }
 
+pub fn ray_march<F>(min: f32, max: f32, step: f32, f: F) -> Option<f32>
+    where F: Fn(f32) -> f32
+{
+    let mut current = min;
+    let mut iteration = 0;
+
+    while current < max {
+        if f(current) <= 0.0 {
+            return Some(current);
+        }
+
+        iteration += 1;
+        current = min + step * iteration as f32;
+    }
+
+    None
+}
+
 #[cfg(test)]
 mod tests {
-    use super::secant;
+    use super::*;
 
     #[test]
     fn secant_test() {
         assert_eq!(secant(10.0, 30.0, 0.001, 32, |x: f32| x.powi(2) - 612.0),
                    Some(24.738634));
         assert_eq!(secant(0.0, 1.0, 0.001, 32, |x: f32| x.powi(2) + 1.0), None);
+    }
+
+    #[test]
+    fn ray_march_test() {
+        assert_eq!(ray_march(0.0, 10.0, 0.1, |x| 5.0 - x), Some(5.0));
+        assert_eq!(ray_march(0.0, 10.0, 0.1, |x| 12.0 - x), None);
     }
 }
